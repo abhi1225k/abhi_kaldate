8b9ade8cab8307974b953bdfc4cb48db5a7bd4ef
Abhishek <dnyaneshwerbarule123@gmail.com>
2025-08-17 19:05:56 +0530

    abhishek12

---
+++ src/Components/MyWork/MyWork.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './MyWork.css'
+import theme_pattern from '../../assets/theme_pattern.svg'
+//import mywork_data from '../../assets/mywork_data'
+
+const MyWork = () => {
+  return (
+    <div className="MyWork">
+      <div className="mywork-title">
+        <h1>My Latest Work</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+      
+    <div className="mywork-container">
+        <p>E-commerce Application Project Description in C#
+            This project involves building a robust E-commerce Application using C# and ASP.NET Core. The application is designed to provide a seamless shopping experience for users while offering efficient management tools for administrators.</p>
+        </div>
+    </div>
+  );
+};
+
+export default MyWork;
